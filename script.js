21fd323f7bd1a17652eb94d9ec0d852aa5c72b94
Franciszek <Franciszek@com>
2019-12-22 21:31:53 +0100

    - etap1

---
+++ script.js
@@ -0,0 +1,15 @@
+const naprawaBtn = document.querySelector('.uslugi');
+
+const uslugiSection = document.querySelector('.samochody');
+
+
+naprawaBtn.addEventListener('click',()=>{
+    console.log(uslugiSection.offsetTop);
+})
+
+
+
+window.addEventListener('scroll', ()=>{
+
+    // console.log(this.scrollY)
+})
